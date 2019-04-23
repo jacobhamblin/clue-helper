9c34b77fd55aa895a8198dc0e06b4000c9b96bc1
Jacob Hamblin <jacob@hamblin.cc>
2019-04-22 22:32:54 -0700

    prepare to switch between game modes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Setup from './Setup';
+import Game from './Gameplay';
 
-class App extends Component () {
-  state = { game: undefined, play: false, setup: undefined }
-  updateSetupState = (state) => this.setState({setup: state})
-  updateGameState = (state) => this.setState({game: state})
+class App extends Component() {
+  state = { game: undefined, play: false, setup: undefined };
+  updateSetupState = state => this.setState({ setup: state });
+  updateGameState = state => this.setState({ game: state });
+  toggleGameState = () => this.setState({ play: !this.state.play });
   render() {
     const { game, play, setup } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <Setup state={setup} reportState={this.updateSetupState} />
-          <Setup state={game} reportState={this.updateGameState} />
+          {play ? (
+            <Game
+              state={game}
+              reportState={this.updateGameState}
+              enterGame={this.toggleGameState}
+            />
+          ) : (
+            <Setup
+              state={setup}
+              reportState={this.updateSetupState}
+              returnToSetup={this.toggleGameState}
+            />
+          )}
         </header>
       </div>
     );
