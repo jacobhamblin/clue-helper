484d5901d8dae67ef4e3726dc4a54313b8fde47c
Jacob Hamblin <jacob@hamblin.cc>
2019-04-23 01:52:27 -0700

    Updates

---
+++ precache-manifest.53e373569c4eeb5ea1ac1b498c3ec893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0fd99bbcb384e827b2adb53fead59d",
+    "url": "/clue-helper/index.html"
+  },
+  {
+    "revision": "87f9c5beb7c50b89d8ad",
+    "url": "/clue-helper/static/css/main.03080d30.chunk.css"
+  },
+  {
+    "revision": "66f395d302fbe0e167c4",
+    "url": "/clue-helper/static/js/2.b6fe4141.chunk.js"
+  },
+  {
+    "revision": "87f9c5beb7c50b89d8ad",
+    "url": "/clue-helper/static/js/main.f7503335.chunk.js"
+  },
+  {
+    "revision": "2e46b60787a60ce3fa7b",
+    "url": "/clue-helper/static/js/runtime~main.fcd7e5db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clue-helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
